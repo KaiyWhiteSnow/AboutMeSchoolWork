93f0c1c6d53d22c9948b7ada9f76266258cf2a19
KaiyWhiteSnow <106684837+KaiyWhiteSnow@users.noreply.github.com>
2023-04-05 12:22:33 +0200

    Half the shit is broken and I don't know what I'm doing

--- BP/main.py
+++ BP/main.py
@@ -1,10 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, request, redirect, url_for, session, render_template
+
+# Define a list of hardcoded users
+users = {"SysAdmin": "Password"}
+username = "SysAdmin"
 
 app = Flask(__name__)
+app.secret_key = 'mysecretkey'  # Set a secret key for session management
 
-@app.route("/")
+@app.route('/')
 def index():
-    return render_template("index.html")
+    # Check if the user is already logged in
+    if "username" in session:
+        return render_template("index.html", username=username)
+    else:
+        return render_template("login.html")
+    
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    # Check if the user is already logged in
+    if 'username' in session:
+        return render_template("index.html")
 
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # Check if the username and password match a hardcoded user
+        if username in users and users[username] == password:
+            session['username'] = username
+            return redirect(url_for('home'))
+        else:
+            return 'Invalid username or password. <a href="/login">Try again</a>'
+
+    return render_template("login.html")
+    
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
